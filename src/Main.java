6e4cd7acd3a279ae7a12ce9abe420060524b86c0
Vally <murkhonovbatoha@gmail.com>
2023-01-19 03:29:50 +0800

    Начало

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,35 @@ public class Main {
     //Задача 1
     public static void task1() {
         System.out.println("Задача 1");
+        int client = 1;
+        if ( client == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (client == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
     //Задача 2
     public static void task2() {
         System.out.println("Задача 2");
+        int client = 1;
+        int clientDeviceYear = 2015;
+        int modelPhone = 2010;
+        if (client == 0 && modelPhone > clientDeviceYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (client == 0 && modelPhone < clientDeviceYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (client == 1 && modelPhone > clientDeviceYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
     }
     //Задача 3
     public static void task3() {
         System.out.println("Задача 3");
+        int year = 2021;
+
+        }
     }
     //Задача 4
     public static void task4() {
