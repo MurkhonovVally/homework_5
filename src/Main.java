f2a35deff6013ede6a745381f5854ef865ba9784
Vally <murkhonovbatoha@gmail.com>
2023-01-19 23:45:13 +0800

    Начало

--- src/Main.java
+++ src/Main.java
@@ -36,15 +36,65 @@ public class Main {
     public static void task3() {
         System.out.println("Задача 3");
         int year = 2021;
+        if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
 
         }
-    }
     //Задача 4
     public static void task4() {
         System.out.println("Задача 4");
+        int deliveryDistance = 95;
+        if (deliveryDistance > 100) {
+            System.out.println("Доставки нет");
+        } else {
+            int days = 1;
+            if (deliveryDistance < 20) {
+                days = days;
+            }
+            if (deliveryDistance > 20) {
+                days = days + 1;
+            }
+            if (deliveryDistance > 60) {
+                days = days + 1;
+            }
+            System.out.println("Потребуется дней: " + days);
+        }
     }
+
     //Задача 5
     public static void task5() {
         System.out.println("Задача 5");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+                System.out.println("Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Autumn");
+                break;
+            case 12:
+                System.out.println("Winter");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+
+        }
+
     }
 }
